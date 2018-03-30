abdf04d3e9e885f6f0ffa8236b171001570fdcdd
kishoremali <kishormali67@gmail.com>
2018-03-30 17:54:42 +0530

    Added for ChangeDiff

---
+++ Changediff/context_diff.py
@@ -0,0 +1,15 @@
+import difflib 
+import sys 
+
+fromfile = sys.argv[0];
+tofile = sys.argv[1];
+out_file_name =sys.argv[2];
+
+out_file = open(out_file_name,"w+");
+fromlines = open(fromfile, 'U').readlines();
+tolines = open(tofile, 'U').readlines();
+
+for line in  difflib.context_diff(fromlines, tolines, fromfile, tofile):
+    out_file.write(line)
+
+out_file.close()
